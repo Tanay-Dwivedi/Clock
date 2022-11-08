2b0ab1271a2b550975395c525c679b0c2a1c3d7f
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2022-11-08 10:08:33 +0530

    time added to the frame

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,17 +1,38 @@
 import javax.swing.*;
 import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MyFrame extends JFrame {
+
+    // adding the Calendar to get current date and time
+    Calendar calendar;
+    SimpleDateFormat simpleDateFormat;
+    // to display the time
+    JLabel jLabel;
+    String time;
 
         // creating constructor for MyFrame
         MyFrame() {
             this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             this.setTitle("CLOCK");
             this.setLayout(new FlowLayout());
-            this.setSize(350, 200);
+            this.setSize(550, 200);
             this.setResizable(false);
+
+            // format of  our time
+            simpleDateFormat = new SimpleDateFormat("hh:mm:ss a  z");
+            // instantiating the jLabel
+            jLabel = new JLabel();
+
+            // getting and storing the time in a string
+            time = simpleDateFormat.format(Calendar.getInstance().getTime());
+            // adding the string to the label
+            jLabel.setText(time);
+
+            //  adding the label to the jFrame
+            this.add(jLabel);
             this.setVisible(true);
 
         }
-
 }
