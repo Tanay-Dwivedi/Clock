64eb70e1877151e06210804fccab90d732a6e8a5
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2022-11-08 10:14:48 +0530

    time updating after every second

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -12,27 +12,42 @@ public class MyFrame extends JFrame {
     JLabel jLabel;
     String time;
 
-        // creating constructor for MyFrame
-        MyFrame() {
-            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            this.setTitle("CLOCK");
-            this.setLayout(new FlowLayout());
-            this.setSize(550, 200);
-            this.setResizable(false);
+    // creating constructor for MyFrame
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("CLOCK");
+        this.setLayout(new FlowLayout());
+        this.setSize(550, 200);
+        this.setResizable(false);
 
-            // format of  our time
-            simpleDateFormat = new SimpleDateFormat("hh:mm:ss a  z");
-            // instantiating the jLabel
-            jLabel = new JLabel();
+        // format of  our time
+        simpleDateFormat = new SimpleDateFormat("hh:mm:ss a  z");
+        // instantiating the jLabel
+        jLabel = new JLabel();
 
+        //  adding the label to the jFrame
+        this.add(jLabel);
+        this.setVisible(true);
+
+        // method tp update our time every 1 second
+        setTime();
+
+    }
+
+    public void setTime() {
+
+        while (true) {
             // getting and storing the time in a string
             time = simpleDateFormat.format(Calendar.getInstance().getTime());
             // adding the string to the label
             jLabel.setText(time);
-
-            //  adding the label to the jFrame
-            this.add(jLabel);
-            this.setVisible(true);
-
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
+
+    }
+
 }
